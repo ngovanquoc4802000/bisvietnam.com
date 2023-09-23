405ec46c076e2cb32249d2a71373970573fb8810
ngovanquoc4802000 <ngovanquoc480@gmail.com>
2023-09-23 23:00:02 +0700

    commit home.js

---
+++ script/home.js
@@ -0,0 +1,23 @@
+(function () {
+  const mockupBannerSlider = [
+    {
+      img: "",
+      title: "",
+      description: "",
+    },
+    {
+      img: "",
+      title: "",
+      description: "",
+    },
+    {
+      img: "",
+      title: "",
+      description: "",
+    },
+  ];
+  function initCarouselInner() {
+
+  }
+  
+})();
